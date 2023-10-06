0673f94634dcf80b72f2c652d3afed9244ae49e1
Rushikesh Bhusal <bhusalrushi056@gmail.com>
2023-10-06 13:06:28 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDFReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pdfFilePath = "file:///D:/Demand_1.pdf";
+
+            using (PdfReader reader = new PdfReader(pdfFilePath))
+            {
+                for (int page = 1; page <= reader.NumberOfPages; page++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string pageText = PdfTextExtractor.GetTextFromPage(reader, page, strategy);
+
+                    Console.WriteLine($"Page {page}:\n{pageText}");
+                }
+            }
+        }
+    }
+}
